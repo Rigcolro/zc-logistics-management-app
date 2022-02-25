a0d28213afa2f03b9ab141115c096295476f5e1f
zou chang <2990057094@qq.com>
2022-02-25 16:25:22 +0800

    🐎 ci:mysql配置更改

--- zc-lm-app-back-end/config/mysql_config.js
+++ zc-lm-app-back-end/config/mysql_config.js
@@ -1,7 +1,7 @@
 const mysqlConfig = {
   user: "root",
-  password: "zc_lm_app_mySql",
-  database: "zc_lm_app_mySql",
+  password: "zc_lm_app_mysql",
+  database: "zc_lm_app_mysql",
   host: "127.0.0.1",
   port: 3306,
 };
